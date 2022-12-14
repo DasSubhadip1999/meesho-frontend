50ebd1fe7ca2d7f522a7e411e45d7ce8bf0d4527
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-12-14 20:02:00 +0530

    tailwindcss with daisyui add to project

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Meesho</title>
+        <title>
+          Meesho - Online Shopping Site for Fashion, Electronics, Home & More |
+          Meesho
+        </title>
         <meta
           name="description"
           content="This is a clone of shopping web application meesho"
