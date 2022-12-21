2e60261e57e11ba595653de4dface151b8519d5a
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-12-21 21:27:40 +0530

    onchange events for product form complete files are pending , added a good feature of discount

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const ProductForm = () => {
+  const [sizeString, setSizeString] = useState([]);
   const [formData, setFormData] = useState({
     manufacturer: "",
     importer: "",
@@ -12,17 +14,83 @@ const ProductForm = () => {
     isKids: false,
     colors: "",
     images: [],
-    price: null,
+    price: 0,
     size: "",
-    discount: null,
+    discount: "",
+    discountedPrice: 0,
   });
+  const [warning, setWarning] = useState(false);
+
+  const {
+    manufacturer,
+    importer,
+    packer,
+    contact,
+    name,
+    type,
+    gender,
+    isKids,
+    colors,
+    images,
+    price,
+    size,
+    discount,
+    discountedPrice,
+  } = formData;
+
+  useEffect(() => {
+    setFormData((prev) => ({ ...prev, size: sizeString.join(",") }));
+  }, [sizeString]);
 
   const onMutate = (e) => {
-    let bool = null;
+    if (!e.target.files) {
+      setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
   };
+
+  const sizeSelect = (e) => {
+    setSizeString((prev) => [...prev, e.target.value]);
+  };
+
+  const setDiscount = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    let discount;
+    if (`${e.target.value}`.includes("%")) {
+      discount = Number((price * e.target.value.split("%")[0]) / 100);
+    } else {
+      discount = Number(e.target.value);
+    }
+
+    if (discount <= price) {
+      setWarning(false);
+      setFormData((prev) => ({ ...prev, discountedPrice: price - discount }));
+    } else {
+      if (!warning) {
+        toast.error("Please check discount");
+      }
+      setWarning(true);
+    }
+  };
+
+  useEffect(() => {
+    if (+discount <= +price) {
+      setWarning(false);
+      setFormData((prev) => ({
+        ...prev,
+        discountedPrice: (price - discount).toFixed(2),
+      }));
+    } else {
+      if (!warning) {
+        toast.error("Please check discount");
+      }
+      setWarning(true);
+    }
+  }, [price]);
 
   const input =
     "border-[1px] border-[rgba(0,0,0,0.2)] rounded-md p-2 my-1 outline-[#f000b8]";
+  const warningInput =
+    "border-2 border-red-500 rounded-md p-2 my-1 outline-red-500 text-red-900 font-bold";
   const inputGroup = "flex flex-col my-2";
   return (
     <>
@@ -36,7 +104,9 @@ const ProductForm = () => {
             className={input}
             type="text"
             name="manufacturer"
+            value={manufacturer}
             placeholder="Enter manufacturer name"
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -45,7 +115,9 @@ const ProductForm = () => {
             className={input}
             type="text"
             name="importer"
+            value={importer}
             placeholder="Enter importer name"
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -53,8 +125,10 @@ const ProductForm = () => {
           <input
             type="text"
             name="packer"
+            value={packer}
             placeholder="Enter packer name"
             className={input}
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -62,8 +136,10 @@ const ProductForm = () => {
           <input
             type="email"
             name="contact"
+            value={contact}
             placeholder="Enter contact email"
             className={input}
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -71,8 +147,10 @@ const ProductForm = () => {
           <input
             type="email"
             name="name"
+            value={name}
             placeholder="Enter contact email"
             className={input}
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -80,8 +158,10 @@ const ProductForm = () => {
           <input
             type="text"
             name="type"
+            value={type}
             placeholder="Enter product type or catagory"
             className={input}
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -93,6 +173,7 @@ const ProductForm = () => {
               value="Male"
               name="gender"
               className="radio radio-secondary"
+              onChange={onMutate}
             />
             <label>Female</label>
             <input
@@ -100,6 +181,7 @@ const ProductForm = () => {
               value="Female"
               name="gender"
               className="radio radio-secondary"
+              onChange={onMutate}
             />
             <label>Others</label>
             <input
@@ -107,6 +189,7 @@ const ProductForm = () => {
               value="others"
               name="gender"
               className="radio radio-secondary"
+              onChange={onMutate}
             />
           </div>
         </div>
@@ -114,7 +197,13 @@ const ProductForm = () => {
           <label>For Kids</label>
           <div className="form-control w-52">
             <label className="cursor-pointer label">
-              <input type="checkbox" className="toggle toggle-secondary" />
+              <input
+                type="checkbox"
+                name="iskids"
+                className="toggle toggle-secondary"
+                onChange={onMutate}
+                value={isKids}
+              />
             </label>
           </div>
         </div>
@@ -122,8 +211,11 @@ const ProductForm = () => {
           <label>Colors Available*</label>
           <input
             type="text"
+            value={colors}
+            name="colors"
             placeholder="Enter comma(,) separated color(red,blue)"
             className={input}
+            onChange={onMutate}
           />
         </div>
         <div className={inputGroup}>
@@ -137,7 +229,37 @@ const ProductForm = () => {
         </div>
         <div className={inputGroup}>
           <label>Price*</label>
-          <input type="number" min={10} className={input} />
+          <input
+            type="number"
+            name="price"
+            min={10}
+            className={input}
+            onChange={onMutate}
+            value={price}
+            placeholder="Enter product price"
+          />
+        </div>
+        <div className={inputGroup}>
+          <label>Discount</label>
+          <input
+            type="text"
+            name="discount"
+            value={discount}
+            onChange={setDiscount}
+            placeholder="You can add discount in % and number"
+            className={warning ? warningInput : input}
+          />
+        </div>
+        <div className={inputGroup}>
+          <label>Discounted Price</label>
+          <input
+            type="number"
+            value={discountedPrice}
+            name="discountedPrice"
+            className={input}
+            readOnly
+            placeholder="Your discounted price will show here"
+          />
         </div>
         <div className={inputGroup}>
           <label>Choose available sizes</label>
@@ -147,41 +269,39 @@ const ProductForm = () => {
               type="checkbox"
               value="S"
               className="checkbox checkbox-secondary"
+              onChange={sizeSelect}
             />
             <label>M</label>
             <input
               type="checkbox"
               value="M"
               className="checkbox checkbox-secondary"
+              onChange={sizeSelect}
             />
             <label>L</label>
             <input
               type="checkbox"
               value="L"
               className="checkbox checkbox-secondary"
+              onChange={sizeSelect}
             />
             <label>XL</label>
             <input
               type="checkbox"
               value="XL"
               className="checkbox checkbox-secondary"
+              onChange={sizeSelect}
             />
             <label>XXL</label>
             <input
               type="checkbox"
               value="XXL"
               className="checkbox checkbox-secondary"
+              onChange={sizeSelect}
             />
           </div>
         </div>
-        <div className={inputGroup}>
-          <label>Discount</label>
-          <input
-            type="text"
-            placeholder="You can add discount in %"
-            className={input}
-          />
-        </div>
+
         <button className="btn bg-[#f000b8] w-full outline-none border-none">
           Add Product
         </button>
