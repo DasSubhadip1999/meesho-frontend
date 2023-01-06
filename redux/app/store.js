bc7a61bd3415b80133c2c349e622107483692ddf
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-01-06 21:33:49 +0530

    place order slice create

--- redux/app/store.js
+++ redux/app/store.js
@@ -3,6 +3,7 @@ import authReduder from "../feature/auth/authSlice";
 import feedbackReducer from "../feature/sellerFeedback/feedbackSlice";
 import productReducer from "../feature/product/productSlice";
 import cartReducer from "../feature/cart/cartSlice";
+import orderReducer from "../feature/order/orderSlice";
 import addressReducer from "../feature/address/addressSlice";
 
 export const store = configureStore({
@@ -11,6 +12,7 @@ export const store = configureStore({
     feedback: feedbackReducer,
     product: productReducer,
     cart: cartReducer,
+    order: orderReducer,
     address: addressReducer,
   },
 });
--- redux/feature/order/orderSlice.js
+++ redux/feature/order/orderSlice.js
@@ -1,6 +1,64 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { placeOrderService } from "./orderService";
 
 const initialState = {
   orders: [],
   order: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+  type: "",
 };
+
+export const placeOrder = createAsyncThunk(
+  "order/place",
+  async (_, thunkAPI) => {
+    let token = thunkAPI.getState().auth.user.token;
+
+    try {
+      return await placeOrderService(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const orderSlice = createSlice({
+  name: "order",
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+      state.type = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(placeOrder.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(placeOrder.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.order = action.payload;
+      })
+      .addCase(placeOrder.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { reset } = orderSlice.actions;
+export default orderSlice.reducer;
