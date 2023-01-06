e4e38fbc5f2a6ac4d58eb8b6a58821d3f5e37c7c
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-01-06 21:06:48 +0530

    order service create

---
+++ redux/feature/order/orderService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const PROXY = "http://localhost:5000";
+
+//@route api/orders/my-orders
+export const placeOrderService = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const res = await axios.post(PROXY + "/api/orders/my-orders", {}, config);
+
+  return res.data;
+};
+
+//@route api/orders/my-orders
+export const getMyOrders = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const res = await axios.get(PROXY + "/api/orders/my-orders", config);
+
+  return res.data;
+};
