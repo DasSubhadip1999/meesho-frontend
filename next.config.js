7f4b69d160ad7c9907ab3e0b985479ee912aad9c
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-01-11 09:32:11 +0530

    nextjs config image config change

--- next.config.js
+++ next.config.js
@@ -1,30 +1,40 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "supplier.meesho.com",
-        pathname: "/images/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.meeshosupplyassets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "meesho-backend.onrender.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.meesho.com",
-        pathname: "/**",
-      },
+    domains: [
+      "supplier.meesho.com",
+      "images.meeshosupplyassets.com",
+      "meesho-backend.onrender.com",
+      "images.meesho.com",
     ],
   },
 };
+
+// images: {
+//   remotePatterns: [
+//     {
+//       protocol: "https",
+//       hostname: "supplier.meesho.com",
+//       pathname: "/images/**",
+//     },
+//     {
+//       protocol: "https",
+//       hostname: "images.meeshosupplyassets.com",
+//       pathname: "/**",
+//     },
+//     {
+//       protocol: "https",
+//       hostname: "meesho-backend.onrender.com",
+//       pathname: "/**",
+//     },
+//     {
+//       protocol: "https",
+//       hostname: "images.meesho.com",
+//       pathname: "/**",
+//     },
+//   ],
+// },
 
 module.exports = nextConfig;
